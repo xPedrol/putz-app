303da1689b9e5b6abee3c6a17821b753fb0d84ea
PedroL <pedrolucassantosferreira426@gmail.com>
2022-07-20 16:44:45 -0300

    ..

--- commonEnvironment.prod.ts
+++ commonEnvironment.prod.ts
@@ -1,13 +1,13 @@
 export const commonEnvironmentProd = {
-  API_URL: 'https://api.putzfilmes.com/api/',
-  API_URL_CONFIG: 'https://api.putzfilmes.com/',
-  IMAGE_URL: 'https://api.putzfilmes.com/api/storage',
+  apiteste_URL: 'https://apiteste.putzfilmes.com/apiteste/',
+  apiteste_URL_CONFIG: 'https://apiteste.putzfilmes.com/',
+  IMAGE_URL: 'https://apiteste.putzfilmes.com/apiteste/storage',
   RENDER_URL: 'https://render.putzfilmes.com/',
   // RENDER_URL: 'http://localhost:3000',
-  RENDER_KEY: 'myapisecret',
+  RENDER_KEY: 'myapitestesecret',
   APP_PUBLIC_TOKEN: '',
-  USER_STANDARD_AVATAR: 'https://api.putzfilmes.com/static/person/member_1_head-256.png',
-  VAPID:
+  USER_STANDARD_AVATAR: 'https://apiteste.putzfilmes.com/static/person/member_1_head-256.png',
+  VapitesteD:
     {
       publicKey: 'BMkk03ZSizZ6H03gABWEYvtd0b_ZUyp5yFWE2yz33wNVcRnHgOOXoTejdsg4kGuCkpRU7-RM3Dv1JX88VIx-e3w',
       privateKey: 'kfipxhi9KAkzQaTPk7xEtSKZvCRMHR4gWR6VCML6NBU'
@@ -16,7 +16,7 @@ export const commonEnvironmentProd = {
   FONT_SIZE_KEY: 'font-size',
   PAGES_APP_URL: 'https://startup.putzfilmes.com',
   MAIN_APP_URL: 'https://projetos.putzfilmes.com',
-  BASE_DOMAIN: '.putzfilmes.com',
+  BASE_DOMAIN: 'putz-app.vercel.app',
   // BASE_DOMAIN: 'localhost',
   AUTH_TOKEN: 'putz_auth_token',
   LOCATION_IQ_KEY: 'pk.e8eb8c3c2b716cef4468ee2acd8c2bf0',
